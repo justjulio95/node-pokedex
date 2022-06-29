aaeb99fce6285e7f95fc534664cd1001ae9365bc
Julio.S <just.julio95@gmail.com>
2022-06-29 12:56:42 -0400

    can successfully pull basic information from pokeAPI including NAME, ID, TYPE(s), and BASE STATS

---
+++ README.md
@@ -0,0 +1,16 @@
+# Random Pokemon Generator
+
+## User Story
+As a pokemon lover, I have a hard time selecting a pokemon. I would like to have an automated random pokemon generator that works at the click of a button.
+
+## Acceptance Criteria
+* Interface should have a pokemon card with all required information.
+    * Pokemon Name
+    * Front and Back Sprite
+    * Pokemon Description
+    * Base Stats
+    * Evolution Chain - with front sprites for each evolution
+* Interface should be responsive and mobile-ready.
+* When clicking on other evolution sprites, should be a link to that evolved pokemon.
+* Should use the [Pokemon API](https://pokeapi.co/) for all data.
+* Anything additional is bonus
--- index.js
+++ index.js
@@ -1,15 +1,55 @@
-// const inquirer = require('inquirer');
 import inquirer from 'inquirer'
+import axios from 'axios'
+import { stat } from 'fs';
 
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Which Pokemon would you like to look up?'
-    }
-  ])
-}
+const baseURL = `https://pokeapi.co/api/v2/`
 
-promptUser()
-.then(pokemon => console.log(pokemon.name))
+axios.get(baseURL + `pokemon/pidgeotto`).then(resp => {
+  const data = resp.data;
+  let typesName = []
+  let statsInfo = []
+
+  const name = data.name;
+  console.log("==================================================================================================================")
+  
+  //getFlavorText(name);
+  
+
+  const id = data.id;
+  const types = data.types;
+  for (let i = 0; i < types.length; i++) {
+    //console.log(types[i].type.name)
+    typesName.push(types[i].type.name)
+  }
+
+  console.log(`${name[0].toUpperCase() + name.substring(1)}, the ${typesName} type Pokemon.`)
+
+  const stats = data.stats
+  for(let i = 0; i < stats.length; i++) {
+    const stat_name = stats[i].stat.name
+    const stat_score = stats[i].base_stat
+
+    statsInfo.push({stat: stat_name, score: stat_score})
+  }
+  console.table(statsInfo)
+})
+.catch(function (error) {
+  if (error.response) {
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    console.log(error.request)
+  } else {
+    console.log('Error', error.message);
+  }
+  console.log(error.config);
+})
+
+
+// function getFlavorText(name) {
+//   axios.get(baseURL + name).then(resp => {
+//     const flavorText = resp.data.flavor_text_entries[0].flavor_text;
+//     console.log(flavorText)
+//   })
+// }
