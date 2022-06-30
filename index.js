b1114ddcf49d49bcd776ca7eb6968efc9b255538
Julio.S <just.julio95@gmail.com>
2022-06-30 12:05:53 -0400

    created conditions to display one or more pokemon in the evolution chain. Need special cases for pokemon like eevee...

--- index.js
+++ index.js
@@ -1,55 +1,132 @@
 import inquirer from 'inquirer'
 import axios from 'axios'
-import { stat } from 'fs';
+import chalk from 'chalk'
+import say from 'say'
+const log = console.log;
 
 const baseURL = `https://pokeapi.co/api/v2/`
 
-axios.get(baseURL + `pokemon/pidgeotto`).then(resp => {
-  const data = resp.data;
-  let typesName = []
-  let statsInfo = []
+// ask the user which pokemon they'd like to look up. 
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Which Pokémon would you like to look up?',
+      name: 'name'
+    }
+  ])
+}
 
-  const name = data.name;
-  console.log("==================================================================================================================")
-  
-  //getFlavorText(name);
-  
+function getBasicInfo(name) {
+  axios.get(baseURL + `pokemon/${name}`).then(resp => {
+    const data = resp.data;
+    let typesName = []
+    let statsInfo = []
 
-  const id = data.id;
-  const types = data.types;
-  for (let i = 0; i < types.length; i++) {
-    //console.log(types[i].type.name)
-    typesName.push(types[i].type.name)
-  }
+    const name = data.name;
+    log(chalk.red("=================================================================================================================="))
 
-  console.log(`${name[0].toUpperCase() + name.substring(1)}, the ${typesName} type Pokemon.`)
+    
+    const types = data.types;
+    for (let i = 0; i < types.length; i++) {
+      typesName.push(types[i].type.name)
+    }
 
-  const stats = data.stats
-  for(let i = 0; i < stats.length; i++) {
-    const stat_name = stats[i].stat.name
-    const stat_score = stats[i].base_stat
+    const speciesURL = data.species.url;
+    getFlavorText(speciesURL)
 
-    statsInfo.push({stat: stat_name, score: stat_score})
-  }
-  console.table(statsInfo)
+    log(chalk.yellow(
+      `${name[0].toUpperCase() + name.substring(1)}`) + ", the " + 
+      chalk.underline.blueBright(`${typesName}`) + " type Pokémon.\n")
+    
+    say.speak(`${name[0].toUpperCase() + name.substring(1)}, the ${typesName} type Pokémon.`)
+
+    const stats = data.stats
+    for(let i = 0; i < stats.length; i++) {
+      const stat_name = stats[i].stat.name
+      const stat_score = stats[i].base_stat
+
+      statsInfo.push({stat: stat_name, score: stat_score})
+    }
+    setTimeout(() => {console.table(statsInfo)}, 2000)
+  })
+  .catch(function (error) {
+    if (error.response) {
+      log('No pokemon with that name... ')
+    }
+  })
+}
+
+
+function getFlavorText(url) {
+  axios.get(url).then(resp => {
+    const evoChainURL = resp.data.evolution_chain.url
+    getEvoChain(evoChainURL);
+
+    const flavorTextArr = resp.data.flavor_text_entries;
+    for (let i = 0; i < flavorTextArr.length; i++) {
+      if (flavorTextArr[i].language.name === 'en') {
+        const flavorText = flavorTextArr[i].flavor_text;
+        const properText = flavorText.split('\n').join(' ').split('\f').join(' ')
+        log(properText + '\n')
+        break;
+      }
+    }
+  })
+  .catch(function (error) {
+    if (error.response) {
+      log(error.response.data);
+      log(error.response.status);
+      log(error.response.headers);
+    } else if (error.request) {
+      log(error.request)
+    } else {
+      log('Error', error.message);
+    }
+    log(error.config);
+  })
+}
+
+function getEvoChain(url) {
+  axios.get(url).then(resp => {
+    let evo1;
+    let evo2;
+    let evo3;
+
+    const evoChain = resp.data.chain;
+
+    evo1 = evoChain.species.name;
+
+    // check to see if there is ONLY a second evolution in the chain. 
+    if (evoChain.evolves_to[0] !== undefined && evoChain.evolves_to[0].evolves_to[0] === undefined) {
+      evo2 = evoChain.evolves_to[0].species.name;
+      console.log(`${evo1} --> ${evo2}`)
+    } 
+    // Check to see if there is a third evolution in the chain. 
+    else if (evoChain.evolves_to[0] !== undefined && evoChain.evolves_to[0].evolves_to[0] !== undefined) {
+
+      evo2 = evoChain.evolves_to[0].species.name;
+      evo3 = evoChain.evolves_to[0].evolves_to[0].species.name;
+
+      console.log(`${evo1} --> ${evo2} --> ${evo3}`)
+    }
+    console.log(`${evo1} does not evolve.`)
+  })
+  .catch(function (error) {
+    if (error.response) {
+      log(error.response.data);
+      log(error.response.status);
+      log(error.response.headers);
+    } else if (error.request) {
+      log(error.request)
+    } else {
+      log('Error', error.message);
+    }
+    log(error.config);
+  })
+}
+
+promptUser()
+.then(pokemon => {
+  getBasicInfo(pokemon.name);
 })
-.catch(function (error) {
-  if (error.response) {
-    console.log(error.response.data);
-    console.log(error.response.status);
-    console.log(error.response.headers);
-  } else if (error.request) {
-    console.log(error.request)
-  } else {
-    console.log('Error', error.message);
-  }
-  console.log(error.config);
-})
-
-
-// function getFlavorText(name) {
-//   axios.get(baseURL + name).then(resp => {
-//     const flavorText = resp.data.flavor_text_entries[0].flavor_text;
-//     console.log(flavorText)
-//   })
-// }
